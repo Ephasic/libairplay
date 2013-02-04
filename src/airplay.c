3124ad345eaa23f01d0ae1160f3489af3d036e94
Aaron Blakely <aaron@ephasic.org>
2013-02-04 14:58:31 -0600

    minor changes.

--- src/airplay.c
+++ src/airplay.c
@@ -9,3 +9,25 @@
 
 #include <airsock.h>
 #include <airplay.h>
+
+#ifndef TRUE
+#define TRUE (-1)
+#endif
+#ifndef FALSE
+#define FALSE (0)
+#endif
+
+// Temporary information
+int kCurrentLogLevel = LOG_INFO;
+int bufferStartFill  = -1;
+
+// Shairport has Win32 support, so
+// I'll copy the macros.  I cannot garuntee
+// libairplay will work on it though.  My focus is
+// OS X and *nix
+
+#ifdef _WIN32
+#define DEVNULL "nul"
+#else
+#define DEVNULL "/dev/null"
+#endif
